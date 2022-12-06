62fcdd4176f033e57b2aa751d95c016769f57784
Carlos Fagiani Junior <fagianijunior@gmail.com>
2022-12-05 23:23:45 -0300

    Add layout

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,5 +1,6 @@
 # Pin npm packages by running ./bin/importmap
 
+pin "jquery", to: "jquery-3.6.1.js", preload: true 
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+church = Church.create ({
+  name: "Maranata",
+  fundation_date: "1980-11-10"
+})
+user = User.create([{
+  name: "Carlos Fagiani Junior",
+  birth_date: "1985-10-28",
+  church: church,
+  email: 'adm@adm.com',
+  password: '1qaz2wsx',
+  password_confirmation: '1qaz2wsx'
+}])
