8b3d16c03e2de85bb940bd6279c58bf08690231e
Carlos Fagiani Junior <fagianijunior@gmail.com>
2023-01-04 02:26:42 -0300

    Add sub kind of to movement, needs change db, dashboard refactored

--- app/models/movement.rb
+++ app/models/movement.rb
@@ -2,9 +2,9 @@ class Movement < ApplicationRecord
   belongs_to :wallet
   belongs_to :user, optional: true
 
-  enum kind_of: [:entrada, :dizimo, :oferta, :entre_contas, :saida] # :entre_contas cria saida na primeira conta e entrada na segunda conta
-
-  validates :kind_of, :amount, :payment_date, :description, :wallet_id, presence: true
+  enum kind_of: { entrada: 1, saida: 2 }
+  enum sub_kind_of: { dizimo: 1, oferta: 2, outras_entradas: 3, entre_contas: 101, compra: 201, funcionario: 201, outros_gastos: 202 }
+  validates :kind_of, :sub_kind_of, :amount, :payment_date, :description, :wallet_id, presence: true
 
 
 end
