180c7ba5e3454c36af56927e6dce3505b9297440
Carlos Fagiani Junior <fagianijunior@gmail.com>
2023-02-04 00:49:25 -0300

    Remove old extrato, fix offer percent

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,7 +1,11 @@
 module DashboardHelper
 
-  def amount_percent(amount, date_range)
+  def tithe_amount_percent(amount, date_range)
     number_to_percentage(amount.to_f / Movement.entrada.dizimo.where(payment_date: date_range).sum(:amount).to_f * 100.0, precision: 0)
+  end
+
+  def offer_amount_percent(amount, date_range)
+    number_to_percentage(amount.to_f / Movement.entrada.oferta.where(payment_date: date_range).sum(:amount).to_f * 100.0, precision: 0)
   end
 
   def list_2022_to_today()
