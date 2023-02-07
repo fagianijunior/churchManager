3b282785c61ba02be7f52345eaed67febe01b2c6
Carlos Fagiani Junior <fagianijunior@gmail.com>
2023-02-07 15:58:13 -0300

    Dashboard layout, rename enums

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -11,14 +11,15 @@ class DashboardController < ApplicationController
       val += accumulator
       accumulator = val
     end
-    
+
     @monthlyInAmountByKind = Movement.entrada.not_entre_contas.where(payment_date: @date_range).group(:sub_kind_of).sum(:amount).sort
     @monthlyOutAmountByKind = Movement.saida.not_entre_contas.where(payment_date: @date_range).group(:sub_kind_of).sum(:amount).sort
 
-    @monthMovements = Movement.not_entre_contas.where(payment_date: @date_range).order(:payment_date)
+    @monthMovements = Movement.not_entre_contas.where(payment_date: @date_range)
+                        .group("date_trunc('day', payment_date)", :kind_of, :sub_kind_of).sum(:amount)
 
-    @monthAmountTithe = Movement.entrada.dizimo.where(payment_date: @date_range).sum(:amount)
-    @monthTithe = Movement.entrada.dizimo.joins(:user).group('users.id').where(payment_date: @date_range)
+    @monthAmountTithe = Movement.entrada.dízimo.where(payment_date: @date_range).sum(:amount)
+    @monthTithe = Movement.entrada.dízimo.joins(:user).group('users.id').where(payment_date: @date_range)
                     .order('sum(movements.amount) DESC').pluck('users.id, users.first_name, users.last_name, sum(movements.amount)')
 
     @monthOffer = Movement.entrada.oferta.where(payment_date: @date_range).order(:payment_date)
@@ -27,7 +28,6 @@ class DashboardController < ApplicationController
     @monthlyMovementsInByCategory = Movement.entrada.not_entre_contas.group(:sub_kind_of).where(payment_date: @date_range).sum(:amount)
     @monthlyMovementsOutByCategory = Movement.saida.not_entre_contas.group(:sub_kind_of).where(payment_date: @date_range).sum(:amount)
 
-    # Show latest five users
     @lastMembers = User.where.not(member_since: nil).order("member_since DESC").take(5)
 
     @monthBirthdays = User.where("EXTRACT(MONTH FROM birth_date) = ?", @month).order(Arel.sql("date_part('day', birth_date)"))
