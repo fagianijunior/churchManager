b8da8a12f9cb2b69239fcf3aeb689ed620c5d608
Carlos Fagiani Junior <fagianijunior@gmail.com>
2023-02-08 23:03:37 -0300

    Extract order by day

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,11 +2,11 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
   before_action :set_header_panel, only: %i[ index ]
   def index
-    @monthMovementsInChart  = Movement.entrada.not_entre_contas.group_by_day(:payment_date).where(payment_date: @date_range).sum(:amount)
-    @monthMovementsOutChart = Movement.saida.not_entre_contas.group_by_day(:payment_date).where(payment_date: @date_range).sum('amount * -1')
+    @monthMovementsInChart  = Movement.entrada.not_entre_contas.where(payment_date: @date_range).group_by_day(:payment_date).sum(:amount)
+    @monthMovementsOutChart = Movement.saida.not_entre_contas.where(payment_date: @date_range).group_by_day(:payment_date).sum('amount * -1')
 
     accumulator = 0
-    @monthMovementsBalanceChart = Movement.group_by_day(:payment_date).where('payment_date <= ?', @date.end_of_month.end_of_day).sum(:amount)
+    @monthMovementsBalanceChart = Movement.where('payment_date <= ?', @date.end_of_month.end_of_day).group_by_day(:payment_date).sum(:amount)
     @monthMovementsBalanceChart.transform_values! do |val|
       val += accumulator
       accumulator = val
@@ -15,18 +15,18 @@ class DashboardController < ApplicationController
     @monthlyInAmountByKind = Movement.entrada.not_entre_contas.where(payment_date: @date_range).group(:sub_kind_of).sum(:amount).sort
     @monthlyOutAmountByKind = Movement.saida.not_entre_contas.where(payment_date: @date_range).group(:sub_kind_of).sum(:amount).sort
 
-    @monthMovements = Movement.not_entre_contas.where(payment_date: @date_range)
+    @monthMovements = Movement.not_entre_contas.where(payment_date: @date_range).order(Arel.sql("date_trunc('day', payment_date)"))
                         .group("date_trunc('day', payment_date)", :kind_of, :sub_kind_of).sum(:amount)
 
     @monthAmountTithe = Movement.entrada.dízimo.where(payment_date: @date_range).sum(:amount)
-    @monthTithe = Movement.entrada.dízimo.joins(:user).group('users.id').where(payment_date: @date_range)
+    @monthTithe = Movement.entrada.dízimo.where(payment_date: @date_range).joins(:user).group('users.id')
                     .order('sum(movements.amount) DESC').pluck('users.id, users.first_name, users.last_name, sum(movements.amount)')
 
     @monthOffer = Movement.entrada.oferta.where(payment_date: @date_range).order(:payment_date)
 
 
-    @monthlyMovementsInByCategory = Movement.entrada.not_entre_contas.group(:sub_kind_of).where(payment_date: @date_range).sum(:amount)
-    @monthlyMovementsOutByCategory = Movement.saida.not_entre_contas.group(:sub_kind_of).where(payment_date: @date_range).sum(:amount)
+    @monthlyMovementsInByCategory = Movement.entrada.not_entre_contas.where(payment_date: @date_range).group(:sub_kind_of).sum(:amount).sort
+    @monthlyMovementsOutByCategory = Movement.saida.not_entre_contas.where(payment_date: @date_range).group(:sub_kind_of).sum(:amount).sort
 
     @lastMembers = User.where.not(member_since: nil).order("member_since DESC").take(5)
 
